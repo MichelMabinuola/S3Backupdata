e18f867d1a21d32c8685f5104f25ee23b18c6d6c
Michael Mabinuola <46244456+MichelMabinuola@users.noreply.github.com>
2023-10-24 17:33:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from backup_script import S3DataBackUp
+import os
 
 if __name__ == "__main__":
-    error_path = "localPath"
-    LOCAL_DIR = 'databackup_dir'
-    bucket_name = "bucketname"
-    region_name = "region"
+    error_path = os.getenv(localPath)
+    LOCAL_DIR = os.getenv(databackup_dir)
+    bucket_name = os.getenv(bucketname)
+    region_name = os.getenv(region)
 
     data_artisan = S3DataBackUp(error_path, LOCAL_DIR, bucket_name, region_name)
     data_artisan.upload_files_to_s3()
